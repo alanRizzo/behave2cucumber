766a5b5fb56246cd051315c596ac7feb2fbdcf06
Alan Rizzo <alan.rizzo@gmail.com>
2025-03-11 12:43:37 -0300

    COM-16 constants fixed

--- src/pepyno/constants.py
+++ src/pepyno/constants.py
@@ -1,22 +1,4 @@
-import tomllib
-from pathlib import Path
+from importlib.metadata import version
 
-
-def get_project_version():
-    config = _get_project_config()
-    return config["project"]["version"]
-
-
-def get_project_name():
-    config = _get_project_config()
-    return config["project"]["name"]
-
-
-def _get_project_config():
-    pyproject_path = Path(__file__).parent.parent.parent / "pyproject.toml"
-    with pyproject_path.open("rb") as f:
-        return tomllib.load(f)
-
-
-NAME = get_project_name()
-VERSION = get_project_version()
+NAME = "pepyno"
+VERSION = version("pepyno")
